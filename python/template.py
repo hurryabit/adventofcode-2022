da75802c5ca51bd0f51821f81a254ebe0c51de07
Martin Huschenbett <martin.huschenbett@posteo.me>
2022-12-10 09:51:33 +0100

    Improve template

--- python/template.py
+++ python/template.py
@@ -3,18 +3,20 @@ from dataclasses import dataclass
 import io
 import re
 
+DAY = 100
 EXAMPLE_INPUT = """"""
+EXAMPLE_OUTPUT = 0
 
 
 def solve(reader: io.TextIOBase) -> int:
     return 0
 
 
-assert solve(io.StringIO(EXAMPLE_INPUT)) == 0
+assert solve(io.StringIO(EXAMPLE_INPUT)) == EXAMPLE_OUTPUT
 
 
 def main():
-    with open("input/day??.txt") as file:
+    with open(f"input/day{DAY}.txt") as file:
         solution = solve(file)
         print(f"{solution}")
 
