b7f6952adf9cca31c0a6d04e5bffcee5c0381459
Martin Huschenbett <martin.huschenbett@posteo.me>
2022-12-10 09:42:08 +0100

    Solutions for day 10 in Python

---
+++ python/day10b.py
@@ -0,0 +1,200 @@
+from __future__ import annotations
+from dataclasses import dataclass
+import io
+import re
+
+EXAMPLE_INPUT = """addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop"""
+
+
+EXAMPLE_OUTPUT = [
+    "##..##..##..##..##..##..##..##..##..##..",
+    "###...###...###...###...###...###...###.",
+    "####....####....####....####....####....",
+    "#####.....#####.....#####.....#####.....",
+    "######......######......######......####",
+    "#######.......#######.......#######.....",
+]
+
+
+def draw(screen, t, reg):
+    x = t % 40
+    y = t // 40
+    screen[y][x] = "#" if abs(x - reg) <= 1 else "."
+
+
+def solve(reader: io.TextIOBase) -> list[str]:
+    reg = 1
+    t = 0
+    screen = [40 * [" "] for _ in range(6)]
+
+    for line in reader.readlines():
+        draw(screen, t, reg)
+        t += 1
+        match line.split():
+            case ["noop"]:
+                pass
+            case ["addx", value]:
+                draw(screen, t, reg)
+                t += 1
+                reg += int(value)
+
+    return list(map(lambda row: "".join(row), screen))
+
+
+assert solve(io.StringIO(EXAMPLE_INPUT)) == EXAMPLE_OUTPUT
+
+
+def main():
+    with open("input/day10.txt") as file:
+        solution = solve(file)
+        for row in solution:
+            print(row)
+
+
+if __name__ == "__main__":
+    main()
